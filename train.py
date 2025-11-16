106beccc74e0d233bc0182bed876825d33d02768
alihuss1017 <alihuss1017@gmail.com>
2025-11-16 15:31:00 -0800

    [ADD] trainer script

--- train.py
+++ train.py
@@ -1,37 +1,34 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import ImageGrid
-import dataset
 import model
 
-builder = dataset.BuildLoaders('data', 0.7, 0.3, 64, 30000)
-train_loader, val_loader = builder.build()
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-max_time_steps = 50
-batch_size = 64
 
-def compute_alphas():
-    t = torch.arange(0, max_time_steps, 1)
+def compute_alphas(num_time_steps: int):
+    t = torch.arange(0, num_time_steps, 1)
     s = 0.008
     f_t = torch.cos((((t / t[-1]) + s) / (1 + s)) * (torch.pi / 2)) ** 2 # (50, )
     alpha_t = f_t / f_t[0] # (50, )
     return alpha_t.cumprod(dim = 0) # (50, )
 
-def sample_alphas(alpha_t):
-    indices = torch.randint(0, max_time_steps, (batch_size, )) # (batch_size, )
+def sample_alphas(num_time_steps: int, alpha_t, batch_size: int):
+    indices = torch.randint(0, num_time_steps, (batch_size, )) # (batch_size, )
     alphas = alpha_t[indices] # (batch size, )
     alphas = alphas.reshape(batch_size, 1, 1, 1) # (batch_size, 1, 1, 1)
     return indices, alphas
 
 class Trainer:
-    def __init__(self, num_epochs: int, lr: float, unet: model.UNet, device: str):
+    def __init__(self, num_epochs: int, lr: float, unet: model.UNet, device: str,
+                 train_loader, val_loader, batch_size: int, num_time_steps: int):
         self.num_epochs = num_epochs
         self.lr = lr
         self.unet = unet.to(device)
         self.device = device
-        self.alpha_t = compute_alphas().to(device)
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.batch_size = batch_size
+        self.num_time_steps = num_time_steps
+        self.alpha_t = compute_alphas(num_time_steps).to(device)
         self.optimizer = optim.Adam(self.unet.parameters(), lr = self.lr)
         self.loss_fn = nn.MSELoss()
 
@@ -39,15 +36,15 @@ class Trainer:
         self.unet.train()
         for epoch in range(self.num_epochs):
             epoch_loss = 0
-            for X in train_loader:
+            for X in self.train_loader:
                 self.optimizer.zero_grad()
 
                 X = X.to(self.device)
                 normalized_X = (X - 127.5) / 127.5
 
-                indices, alphas = sample_alphas(self.alpha_t)
+                indices, alphas = sample_alphas(self.num_time_steps, self.alpha_t, self.batch_size)
 
-                eps = torch.normal(0, 1, size = normalized_X.shape).to(device)
+                eps = torch.normal(0, 1, size = normalized_X.shape).to(self.device)
                 noisy_images = torch.sqrt(alphas) * normalized_X + torch.sqrt(1 - alphas) * eps
                 noise = self.unet(noisy_images, timesteps = indices)
 
@@ -57,7 +54,7 @@ class Trainer:
 
                 self.optimizer.step()
             
-            print(f'Epoch {epoch + 1}: Loss: {epoch_loss / len(train_loader)}')
+            print(f'Epoch {epoch + 1}: Loss: {epoch_loss / len(self.train_loader)}')
 
     def save_model(self):
         torch.save(self.unet.state_dict(), 'model.pt')
